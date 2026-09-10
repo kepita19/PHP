a6e328680130f7545dcaa9f4f59d5e6df58e9af4
kepita19 <k_izaguirrefer@fpzornotzalh.eus>
2026-09-10 10:14:31 +0200

    lehenengo commit

---
+++ ariketak1/01ariketa.php
@@ -0,0 +1,20 @@
+<?php
+$zembaki1 = 5;
+$zembaki2 = 3;
+$suma = 0;
+$resta = 0;
+$multi = 0;
+
+$suma = $zembaki1 + $zembaki2;
+$resta = $zembaki1 - $zembaki2;
+$multi = $zembaki1 * $zembaki2;
+$zati = $zembaki1 / $zembaki2;
+$modulua = $zembaki1 % $zembaki2;
+echo("Lehenego zembakia ". $zembaki1. " eta bigarren zembakia ". $zembaki2);
+echo("<br> gehiketa ". $suma);
+echo("<br> Kenketa ". $resta);
+echo("<br> Multi ". $multi);
+echo("<br> zati ". $zati);
+echo("<br> Modulo ". $modulua);
+
+?>
